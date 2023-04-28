9a0bc1158b870569d5cffd59395b2c47c5abf486
Nicolai Hartvig Sørensen <nicolaihs@gmail.com>
2023-04-28 13:39:29 +0200

    Added /bulklang endpoint returning language scores for all words in words= parameter, and lang_count of all first languages
    #4

--- lexiscore/app.py
+++ lexiscore/app.py
@@ -1,15 +1,27 @@
 """FastAPI service for wordres."""
+from collections import defaultdict
 from fastapi import Depends, FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, PlainTextResponse
 from fastapi_simple_security import api_key_router, api_key_security
-
+from os import environ
+from typing import List
 from lexiscore import CONFIG, logger
 from lexiscore.main import (
     load_languages,
     calculate_word_probability,
     rank_all_languages,
 )
+
+enable_security = environ.get("ENABLE_SECURITY", "").strip()
+if enable_security is None:
+    raise ValueError("ENABLE_SECURITY not set")
+enable_security = enable_security.lower() in ("true", "1") and True or False
+security_str = (
+    "\033[1;32mENABLED\033[0m" if enable_security else "\033[1;31mDISABLED\033[0m"
+)
+logger.info(f"Security: {security_str}")
+
 
 title = CONFIG.get("general", "title")
 
@@ -72,3 +84,51 @@ async def rank_languages(
     result = await rank_all_languages(word, probabilities, langs=langs)
     result = [(lang, score) for lang, score in result if score >= threshold]
     return JSONResponse(content=result)
+
+
+@app.get(
+    "/bulklang",
+    response_class=JSONResponse,
+    dependencies=[Depends(api_key_security)],
+)
+async def bulk_rank_languages(
+    words: str, threshold: float = 0.000001, languages: str | None = None
+) -> JSONResponse:
+    """Rank the languages for each word in words, only return languages with score > threshold.
+
+    - words: str. Comma-separated list of words to check.
+    - threshold: float. Minimum score to return a language.
+    - languages: str. Comma-separated list of languages to rank. If None, all languages are ranked.
+    """
+    word_list = words.split(",")
+    word_list = [word.strip() for word in words]
+    if languages is not None:
+        langs = languages.split(",")
+    else:
+        langs = None
+    result = []
+    for word in word_list:
+        word_result = await rank_all_languages(word, probabilities, langs=langs)
+        word_result = [
+            (lang, score) for lang, score in word_result if score >= threshold
+        ]
+        result.append(word_result)
+    # Counts the number of times each langauge is the first result
+    lang_count = {}
+    for word_result in result:
+        if len(word_result) > 0:
+            lang = word_result[0][0]
+            lang_count[lang] = lang_count.get(lang, 0) + 1
+    # Sorts the languages by the number of times they are the first result
+    lang_count = sorted(lang_count.items(), key=lambda x: x[1], reverse=True)
+
+    output = {
+        "words": words,
+        "results": result,
+        "lang_count": lang_count,
+    }
+    return JSONResponse(content=output)
+
+
+if not enable_security:
+    app.dependency_overrides[api_key_security] = lambda: None
--- lexiscore/tests/test_app.py
+++ lexiscore/tests/test_app.py
@@ -2,6 +2,21 @@
 from typing import assert_type
 import pytest
 import requests
+from fastapi.testclient import TestClient
+from lexiscore.app import app
+
+# TODO: Change tests to TestClient
+
+
+@pytest.fixture(scope="session")
+async def test_app():
+    # Load the test client
+    async with TestClient(app) as client:
+        # Call the startup event
+        await app.startup()
+        # Return the test client
+        yield client
+
 
 HOST = "http://127.0.0.1:8000"
 THRESHOLD = 0.0001
@@ -44,10 +59,49 @@ def test_check_invalid_word() -> None:
 def test_language_ranking() -> None:
     """Test the language ranking endpoint."""
     word = "husene"
-    url = f"{HOST}/lang/{word}"
+    url = f"{HOST}/lang/{word}?languages=da,de,eng"
     response = requests.get(url)
     json = response.json()
     assert isinstance(json, list) == True
     assert len(json) > 1
     assert json[0][0] == "da"
     assert json[0][1] > THRESHOLD
+
+
+# TODO: Fix this test
+async def test_bulklang_endpoint_with_no_languages_provided():
+    """
+    GIVEN a FastAPI app
+    WHEN the '/bulklang' endpoint is hit with a list of words and no languages provided
+    THEN ensure the endpoint returns a JSON response with correct results and status code
+    """
+    # Define the input data
+    input_data = {"words": "apple,banana,orange,pear"}
+
+    # Hit the endpoint
+    response = await test_app.get("/bulklang", params=input_data)
+
+    # Ensure the status code is correct
+    assert response.status_code == 200
+
+    # Ensure the response content is correct
+    response_content = response.json()
+    assert "words" in response_content
+    assert "results" in response_content
+    assert "lang_count" in response_content
+
+    # Ensure the word list is correct
+    assert response_content["words"] == input_data["words"]
+
+    # Ensure the results are correct
+    expected_results = [
+        [("English", 0.9), ("French", 0.1)],
+        [("Spanish", 0.8), ("Portuguese", 0.2)],
+        [("English", 0.7), ("Dutch", 0.3)],
+        [("English", 1.0)],
+    ]
+    assert response_content["results"] == expected_results
+
+    # Ensure the language count is correct
+    expected_lang_count = [("English", 3), ("Spanish", 1)]
+    assert response_content["lang_count"] == expected_lang_count
