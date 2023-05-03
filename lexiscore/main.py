f9fa6459b0a5bb1ac14a46ba7bcec1f6ba1b9b5c
Nicolai Hartvig Sørensen <nicolaihs@gmail.com>
2023-05-03 11:33:08 +0200

    Updated docstrings in main.py
    #8

--- lexiscore/main.py
+++ lexiscore/main.py
@@ -9,6 +9,14 @@ from lexiscore import CONFIG, logger, async_timeit
 
 @async_timeit
 async def load_languages(force_training: bool = False) -> Dict[str, Dict[str, float]]:
+    """Load and train all languages from the config file.
+
+    Args:
+        force_training: force training, otherwise pickled versions are used if they are present. Defaults to False.
+
+    Returns:
+        a dictionary with the languages keys and propability dictionaries as values.
+    """
     logger.info(f"Loading languages")
     languages = CONFIG.get("general", "languages").split(",")
 
@@ -22,9 +30,18 @@ async def load_languages(force_training: bool = False) -> Dict[str, Dict[str, fl
 
 @async_timeit
 async def get_probabilties(lang: str, force_training: bool = False) -> Dict[str, float]:
+    """Return probabilities for a given language.
+
+    Args:
+        lang: the language to get probabilities for. The language must be present in the config file with a filename supplied.
+        force_training: force training, otherwise pickled versions are used if they are present. Defaults to False.
+
+    Returns:
+        a dictionary with the character ngrams as keys and the probabilities as values.
+    """
     logger.info(f"Getting probabilities for {lang}")
-    # Try to unpickle trigram probabilities. If that fails, train the splitter
-    # and pickle the trigram probabilities.
+    # Try to unpickle ngram probabilities. If that fails, train the splitter
+    # and pickle the ngram probabilities.
     pickle_file = os.path.join(tempfile.gettempdir(), f"lexiscore_probs_{lang}.pickle")
 
     if not force_training:
@@ -52,8 +69,17 @@ async def get_probabilties(lang: str, force_training: bool = False) -> Dict[str,
 async def calculate_ngram_probs(
     corpus_file: str, lower: bool = True, ngram_length: int = 4
 ) -> Dict[str, float]:
-    """Calculate n-gram probabilities for a corpus."""
-    # Create an empty dictionary to store trigram counts
+    """Calculate n-gram probabilities for a corpus.
+
+    Args:
+        corpus_file: the filepath to a file with a list of words
+        lower: lowercase all words. Defaults to True.
+        ngram_length: the length of the ngram to use. Defaults to 4.
+
+    Returns:
+        a dictionary with the ngrams as keys and the probabilities as values.
+    """
+    # Create an empty dictionary to store ngram counts
     ngram_counts = defaultdict(int)
 
     # Initialize the total count to zero
@@ -73,17 +99,12 @@ async def calculate_ngram_probs(
                 ngram_counts[ngram] += 1
 
     # # Calculate ngram probabilities
-    # ngram_probs = {}
-    # for ngram, count in ngram_counts.items():
-    #     ngram_probs[ngram] = count / total_count
-
     # Set smoothing parameter
     k = 100
 
     logger.info(f"Smoothing parameter (Laplace): {k} (total_count: {total_count})")
 
     # Calculate ngram probabilities with Laplace smoothing
-    # (jeg ved ikke hvad Laplace er, men det siger chatgpt...)
     ngram_probs = {}
     for ngram, count in ngram_counts.items():
         ngram_probs[ngram] = (count + k) / (total_count + k * len(ngram_counts))
@@ -94,12 +115,23 @@ async def calculate_ngram_probs(
 async def calculate_word_probability(
     word: str, ngram_probs: dict, lower: bool = True, ngram_length: int = 4
 ) -> float:
+    """Generate the probability of a word using ngram probabilities.
+
+    Args:
+        word: the word to calculate the probability for
+        ngram_probs: the ngram probabilities to use
+        lower: lowercase the word. Defaults to True.
+        ngram_length: the length of the ngram to use. Defaults to 4.
+
+    Returns:
+        the probability of the word
+    """
     # Add $ before and after the word depending on ngram_length
     word = "$" * (ngram_length - 1) + word + "$" * (ngram_length - 1)
     if lower:
         word = word.lower()
 
-    # Calculate the probability of each trigram and multiply them together
+    # Calculate the probability of each ngram and multiply them together
     log_output = []
     word_prob = 1.0
     for i in range(len(word) - (ngram_length - 1)):
@@ -118,7 +150,6 @@ async def calculate_word_probability(
 
     # Normalize the probability by word length
     word_prob = word_prob ** (1.0 / max(1, len(word) - 2))
-    print(word_prob)
     return word_prob
 
 
@@ -126,11 +157,20 @@ async def calculate_word_probability(
 async def rank_all_languages(
     word: str, probs: dict, langs: List[str] | None = None
 ) -> List[Tuple[str, float]]:
+    """Use probabilities to rank languages for a word.
+
+    Args:
+        word: the word to rank
+        probs: a dictionary with probabilities for each language
+        langs: the languages to consider, if None all languages are considered. Defaults to None.
+
+    Returns:
+        a list of tuples with the language and the probability for the word in that language
+    """
     result = []
     for lang in probs:
         if langs and lang not in langs:
             continue
-        print(lang, word)
         score = await calculate_word_probability(word, probs[lang])
         if "-" in word:
             score_mod = await calculate_word_probability(
